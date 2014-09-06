6e643fa68baeb94ef26b87b9a3b4fe8f69b582e6
alex9446 <alex9446@gmail.com>
2014-09-06 20:19:31 +0200

    Change theme and add AppStat log

--- src/com/alex/lions_amaranto/MainActivity.java
+++ src/com/alex/lions_amaranto/MainActivity.java
@@ -2,6 +2,7 @@ package com.alex.lions_amaranto;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +12,50 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		Log.e("AppStat", "exec onCreate");
+		
+	}
+	
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		Log.w("AppStat", "exec onStart");
+	}
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.i("AppStat", "exec onResume");
+	}
+	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		Log.d("AppStat", "exec onPause");
+	}
+	
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		Log.w("AppStat", "exec onStop");
+	}
+	
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+		Log.i("AppStat", "exec onRestart");
+	}
+	
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.e("AppStat", "exec onDestroy");
 	}
 
 }
